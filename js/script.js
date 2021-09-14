7406fdae1671b02f4b4ac65e637d6e4878f41ffc
Helene <hcblackmer@gmail.com>
2021-09-13 18:41:02 -0700

    Added modal for test customization.

---
+++ js/script.js
@@ -0,0 +1,58 @@
+$(function () {
+    $('#testButton').click(function () {
+        $('#testModal').modal('show');
+    });
+});
+
+let currentTab = 0;
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    showTab(currentTab);
+});
+
+function showTab(n) {
+    let x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = '<i class="fa fa-angle-double-right"></i>';
+    } else {
+        document.getElementById("nextBtn").innerHTML = '<i class="fa fa-angle-double-right"></i>';
+    }
+    fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+    let x = document.getElementsByClassName("tab");
+    if (n == 1 && !validateForm()) return false;
+    x[currentTab].style.display = "none";
+    currentTab = currentTab + n;
+    if (currentTab >= x.length) {
+        document.getElementById("nextprevious").style.display = "none";
+        document.getElementById("all-steps").style.display = "none";
+        document.getElementById("register").style.display = "none";
+        document.getElementById("text-message").style.display = "block";
+    }
+    showTab(currentTab);
+}
+
+function validateForm() {
+    let x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    } return valid;
+}
+
+function fixStepIndicator(n) {
+    let i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    } 
+    ßx[n].className += " active";
+}
