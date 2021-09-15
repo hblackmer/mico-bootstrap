75932f43f7106a9e46de1b2e5881db8b93d9fe3f
Helene <hcblackmer@gmail.com>
2021-09-15 16:22:14 -0700

    Update link animation and clean up typing animation.

--- js/typing.js
+++ js/typing.js
@@ -1,14 +1,11 @@
-const textEl = document.getElementById('header-name');
 const text = 'MICO';
 let idx = 1;
-let speed = 300;
-
-writeText();
 
 function writeText() {
-    textEl.innerText = text.slice(0, idx)
-
+    document.getElementById('header-name').innerText = text.slice(0, idx)
     idx++
 
-    setTimeout(writeText, speed)
+    setTimeout(writeText, 300)
 }
+
+writeText();
